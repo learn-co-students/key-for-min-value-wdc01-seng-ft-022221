73b9f4c6a9ba221edf874c556e885503f4ea0a3d
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 04:24:12 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  dolphin = 333333
+  whale = nil
+  name_hash.each do |key, value|
+    if value < dolphin
+      dolphin = value
+      whale = key
+  end
+end
+  whale
+  whale
 end
